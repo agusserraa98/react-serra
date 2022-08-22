121cc253472e913580a32f2ad887b2506d4e7974
agusserraa98 <agustin.sebastian.serra@gmail.com>
2022-08-21 23:22:43 -0300

    Se agrego navbar-mui-material

---
+++ install.sh
@@ -0,0 +1,3 @@
+cd react-serra/
+npm install @mui/material @emotion/react @emotion/styled
+npm install @mui/material @mui/styled-engine-sc styled-components
---
+++ react-serra/src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  Stack,
+  Button,
+} from "@mui/material";
+import AnchorIcon from "@mui/icons-material/Anchor";
+
+export const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          El Ancla store
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit">SOBRE NOSOTROS</Button>
+          <Button color="inherit" id="resources-button">
+            Resources
+          </Button>
+          <Button color="inherit">MI CUENTA</Button>
+          <Button color="inherit">MI CARRITO</Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+};
