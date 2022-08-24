379bdfe49e58167cc0d111a936cdbf626623ad67
agusserraa98 <agustin.sebastian.serra@gmail.com>
2022-08-24 00:12:40 -0300

    Actividad clase 3

--- react-serra/src/App.tsx
+++ react-serra/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
-import { Navbar } from './components/Navbar';
+import ListContainer from './components/listContainer/ListContainer';
+import { Navbar } from './components/navbar/navBar.tsx/Navbar';
+
+
+
+
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <ListContainer saludo={'BIENVENIDO'}></ListContainer>
     </div>
   );
 }
-
 export default App;
---
+++ react-serra/src/components/listContainer/ListContainer.tsx
@@ -0,0 +1,12 @@
+const ListContainer = ({saludo}) => {
+    
+    return(
+        <div>
+            <h1>¡{saludo}!</h1>
+            <p>Esta es nuestra practica de REACT!!</p>
+        </div>
+    )
+}
+
+
+export default ListContainer
